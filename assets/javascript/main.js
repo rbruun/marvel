6ae6f8d49396d31c72e62f8816ba1f3d27eb74e4
Ron Bruun <ronald.bruun@libertymutual.com>
2017-06-06 15:43:44 -0400

    fixed issue with click event firing multiple times

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,7 +6,7 @@
         let searchVal = $("#nameSearch");
         let mainrow = $(".mainrow");
 
-        // this functioin calls when the submit button is clicked
+        // this function calls when the submit button is clicked
         search.click(function () {
 
             mainrow.html("");
@@ -39,44 +39,42 @@
                         val.thumbnail.path + "." + val.thumbnail.extension + "'></div></div>");
                 })
             });
+        });
 
+        // have to do this way because the 'click' event isn't recognized for dynamically added elements
+        // this event is triggered when an image is clicked on the character list
+        mainrow.on("click", ".image", function () {
+            console.log("inside click event");
 
-            // have to do this way because the 'click' event isn't recognized for dynamically added elements
-            // this event is triggered when an image is clicked on the character list
-            $(document).on("click", ".image", function () {
-                console.log("inside click event");
+            // remove any event details if there are any currently in the DOM
+            $(".events").remove();
 
-                // remove any event details if there are any currently in the DOM
-                $(".events").remove();
+            // find the parent div of the one clicked, the events will be placed following this div
+            outerdiv = $(this).parent();
 
-                // find the parent div of the one clicked, the events will be placed following this div
-                outerdiv = $(this).parent();
+            let newstuff = "<div class='events'>";
 
-                let newstuff = "<div class='events'>";
-
-                // call the api to get the events of the character that was clicked on
-                let apiCall = $.get("https://gateway.marvel.com:443/v1/public/characters/" + $(this).attr("id") + "/events?limit=100" + "&ts=1&apikey=d66256f365e1f2c00d8cdc9a2fb7ddc9&hash=453f059b2ec8796b0ad8b40fa4adc8cc", function (data) {
-                    if (parseInt(data.data.count) > 0) {
-                        $.each(data.data.results, function (index, val) {
-                            newstuff += "<div class='event'>"
-                            newstuff += "<div class='image'><img height='100px' src='" +
-                                val.thumbnail.path + "." + val.thumbnail.extension + "'></div>";
-                            newstuff += "<div class='eventDetail'>";
-                            newstuff += "<div><span class='lbl'>Event Title: </span>" + val.title + "</div>";
-                            newstuff += "<div><span class='lbl'>Event Description: </span>" + val.description + "</div>";
-                            newstuff += "<div><span class='lbl'>Characters Involved: </span>" + val.characters.available + "</div>";
-                            newstuff += "</div></div>"
-                        });
-                    } else {
-                        newstuff += "this character has no events"
-                    }
-                });
-                apiCall.always(function () {
-                    newstuff += "</div>"
-                    outerdiv.after(newstuff);
-                })
+            // call the api to get the events of the character that was clicked on
+            let apiCall = $.get("https://gateway.marvel.com:443/v1/public/characters/" + $(this).attr("id") + "/events?limit=100" + "&ts=1&apikey=d66256f365e1f2c00d8cdc9a2fb7ddc9&hash=453f059b2ec8796b0ad8b40fa4adc8cc", function (data) {
+                if (parseInt(data.data.count) > 0) {
+                    $.each(data.data.results, function (index, val) {
+                        newstuff += "<div class='event'>"
+                        newstuff += "<div class='imageevent'><img height='100px' src='" +
+                            val.thumbnail.path + "." + val.thumbnail.extension + "'></div>";
+                        newstuff += "<div class='eventDetail'>";
+                        newstuff += "<div><span class='lbl'>Event Title: </span>" + val.title + "</div>";
+                        newstuff += "<div><span class='lbl'>Event Description: </span>" + val.description + "</div>";
+                        newstuff += "<div><span class='lbl'>Characters Involved: </span>" + val.characters.available + "</div>";
+                        newstuff += "</div></div>"
+                    });
+                } else {
+                    newstuff += "this character has no events"
+                }
             });
-
+            apiCall.always(function () {
+                newstuff += "</div>"
+                outerdiv.after(newstuff);
+            })
         });
 
     });
